883a3a5dc29179f7419a92f46c1076fe7db83ece
Bingil35 <nguyenvanbinh352005@gmail.com>
2025-05-09 15:51:40 +0700

    Add loyal_customer.py

---
+++ models/loyal_customer.py
@@ -0,0 +1,23 @@
+from models.customer import Customer
+
+class LoyalCustomer(Customer):
+    def __init__(self, customer_id, name, phone, email):
+        super().__init__(customer_id, name, phone, email)
+        self.loyalty_points = 0
+
+    def add_loyalty_points(self, points):
+        if points > 0:
+            self.loyalty_points += points
+        # else:
+        #     raise ValueError("Loyalty points must be positive")
+
+    def redeem_loyalty_points(self, points):
+        if 0 < points <= self.loyalty_points:
+            self.loyalty_points -= points
+        # else:
+        #     raise ValueError("Invalid number of loyalty points to redeem")
+
+    def get_type(self):
+        return "Loyal"
+    def __str__(self):
+        return f"{super().__str__()}| Loyalty Points: {self.loyalty_points}| Type: {self.get_type()}"
